b91907b06744092bff65533d97308e4e6cd4ef85
eldiyar.ukenov <eldiyar.ukenov@optimabank.kg>
2025-10-29 09:34:00 +0600

    AstTreeGeneratedPropertyKey.kt delate

--- prometey-ast-tree/ast-tree-compiler-plugin/src/main/kotlin/org/prometey/ast/tree/compiler/plugin/shared/AstTreeGeneratedPropertyKey.kt
+++
@@ -1,8 +0,0 @@
-package org.prometey.ast.tree.compiler.plugin.shared
-
-import org.jetbrains.kotlin.GeneratedDeclarationKey
-import org.jetbrains.kotlin.name.FqName
-
-class AstTreeGeneratedPropertyKey(
-    sourceName: FqName
-) : GeneratedDeclarationKey()
